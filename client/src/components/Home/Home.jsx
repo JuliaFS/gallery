4062551c9b5a9fcc4ab536542bfe6ba7a9382c24
Yulia <yuliya.f.s@gmail.com>
2023-11-14 11:28:29 +0200

    added some pictures to img folder

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+    return (
+        <div>
+            <div>
+                <img src="" />
+            </div>
+            <h3>title</h3>
+            <div>
+                <p>Some text here...</p>
+            </div>
+            <div>
+                <a>Details</a>
+                <button>Like</button>
+            </div>
+        </div>
+    );
+}
