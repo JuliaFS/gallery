0abb69b36c0d8183ce0e7040ac0f3ac2a6541a88
Yulia <yuliya.f.s@gmail.com>
2023-11-14 23:26:47 +0200

    modify requester

--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -1,6 +1,19 @@
+const buildOptions = (data) => {
+    const options = {};
+
+    if(data){
+        options.body = JSON.stringify(data);
+        options.header = {
+            'content-type': 'application/json'
+        }
+    }
+    return options;
+}
+
 export const request = async (method, url, data) => {
     const response = await fetch(url, {
         method,
+        ...buildOptions(data)
     });
 
     // if(!response.ok){
@@ -10,3 +23,6 @@ export const request = async (method, url, data) => {
     const result = await response.json();
     return result;
 };
+
+//pravi se po analogiq za vsichki metodi, za da se izvikva s metoda - request.get(parametri)
+//export const get = request.bind(null, 'GET')
--- client/src/services/pictureService.js
+++ client/src/services/pictureService.js
@@ -10,15 +10,10 @@ export const getAll = async () => {
 
 
 export const create = async (data) => {
-    const response = await fetch(`${baseUrl}`,{
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
+    const response = await request('POST', baseUrl, data);
 
     const result = await response.json();
 
     return result;
 };
+
