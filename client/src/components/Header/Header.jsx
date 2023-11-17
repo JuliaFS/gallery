f11b27d197fcb77baa8089822a7681c5dea1aa9a
Yulia <yuliya.f.s@gmail.com>
2023-11-17 08:52:16 +0200

    changes in navigation links

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -8,12 +8,8 @@ export default function Header(){
             <div>
             <h2><Link className="home" to={paths.home}>Art gallery</Link></h2>
             <nav>
-                <Link to={paths.registerPainter}>Enter for painters</Link>
-                <div id="painters">
-                    <Link to={paths.registerPainter}>Register painter</Link>
-                    <Link to={paths.createPicture}>Create picture</Link>
-                </div>
                 <div id="user">
+                    <Link to={paths.createPicture}>Create picture</Link>
                     <Link to={paths.gallery}>All pictures</Link>
                     <Link to={paths.logout}>Logout</Link>
                 </div>
