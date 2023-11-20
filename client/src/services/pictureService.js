0bbe64a170cd8c6106a7cde956f925d93a0470f2
Yulia <yuliya.f.s@gmail.com>
2023-11-21 00:38:05 +0200

    changed baseUrl in picture Service

--- client/src/services/pictureService.js
+++ client/src/services/pictureService.js
@@ -1,11 +1,11 @@
 import * as request from "../lib/request";
 
-const baseUrl = "http://localhost:3030/jsonstore/pictures";
+const baseUrl = "http://localhost:3030/data/pictures";
 
 export const getAll = async () => {
     const result = await request.get(baseUrl);
-    //object in object
-    return Object.values(result);
+    
+    return result;
 };
 
 export const getOne = async (pictureId) => {
