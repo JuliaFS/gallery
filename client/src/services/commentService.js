5ec75a7c3da3102b0b01cbb3d774fdc1fc870ea2
Yulia <yuliya.f.s@gmail.com>
2023-11-21 00:50:47 +0200

    corect working comments

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -1,16 +1,17 @@
 import * as request from '../lib/request';
 
-const baseUrl = 'http://localhost:3030/jsonstore/comments';
+const baseUrl = 'http://localhost:3030/data/comments';
 
 export const getAll = async (pictureId) => {
     const query = new URLSearchParams({
         where: `pictureId="${pictureId}"`
     });
 
-    const result = await request.get(baseUrl);
+    const result = await request.get(`${baseUrl}?${query}`);
 
     // TODO: temp solution until migration to collections service 
-    return Object.values(result).filter(comment => comment.pictureId === pictureId);
+    //return result.filter(comment => comment.pictureId === pictureId);
+    return result;
 };
 
 export const create = async (pictureId, username, text) => {
