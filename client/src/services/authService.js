fc6cc8ad595c62398ce26aa9a9b6df7190a3e2ec
Yulia <yuliya.f.s@gmail.com>
2023-12-03 12:11:17 +0200

    test authService login without cretaed user on server

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -3,13 +3,15 @@ import * as request from "../lib/request";
 const baseUrl = 'http://localhost:3030/users';
 
 export const login = async (email, password) => {
+    try{
     const result = await request.post(`${baseUrl}/login`, {
         email,
         password
     });
-
-    console.log('result: ' + result + 'in login ');
     return result;
+    } catch(err){
+        console.log(err.message);
+    }
 }
 
 export const register = async (email, password, confirmPassword) => {
