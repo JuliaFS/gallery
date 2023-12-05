d996ccf3a53603e9064d0abe1eee50d017f17226
Yulia <yuliya.f.s@gmail.com>
2023-12-05 18:37:19 +0200

    moved validate function to common folder

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import AuthContext from '../../contexts/AuthContext';
 import useForm from '../../hooks/useForm';
+import validate from '../common/validateRegisterForm';
 
 import { Path } from '../../constants/constants';
 import styles from './Register.module.css';
@@ -34,37 +35,6 @@ export default function Register() {
         [RegisterFormKeys.Password]: '',
         [RegisterFormKeys.ConfirmPassword]: ''
     })
-
-    const validate = (value) => {
-        const errors = {};
-        const curPassword = value.password;
-        //console.log('validate: ' + value);
-       // const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-        if(!value.email){
-            errors.email = 'Email is required!';
-        } else if(!regex.test(value.email)){
-            errors.email = 'This is not a valid email format';
-        }
-        if(!value.password){
-            errors.password = 'Password is required!';
-        } else if(value.password.length < 4){
-            errors.password = 'Password must be at least 4 character long';
-        }else if(value.password.length > 15){
-            errors.password = 'Password can not exceed more than 15 characters';
-        }
-        if(!value["confirm-password"]){
-            errors["confirm-password"] = 'Confirming password is required';
-        } else if (curPassword !== value["confirm-password"]){
-            errors["confirm-password"] = 'Password do not match';
-        }
-        // } else if(value["confirm-password"].length < 4){
-        //     errors["confirm-password"]= 'Password must be at least 4 character long';
-        // }else if(value["confirm-password"].length > 15){
-        //     errors["confirm-password"] = 'Password can not exceed more than 15 characters';
-        //}
-        return errors;
-    }
 
     const validateInput = (e) =>{
         //console.log(values)
