8d3637518d4779c9452f2e39d59df681af1c7847
Yulia <yuliya.f.s@gmail.com>
2023-12-05 11:36:24 +0200

    Revert "Update CreatePicture.jsx"
    This reverts commit 146465797ce0d3f451e83bde82b0fdd8b4e62c9a.

--- client/src/components/CreatePicture/CreatePicture.jsx
+++ client/src/components/CreatePicture/CreatePicture.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom';
 
 import * as pictureService from '../../services/pictureService';
 import { Path } from '../../constants/constants';
+import validate from './validateCreatePicture';
+import Modal from '../404/ModalErrors';
 
 import styles from './CreatePicture.module.css';
 
@@ -11,7 +13,7 @@ const formInitialState = {
     title: '',
     category: '',
     painter: '',
-    painterAge: 0,
+    painterAge: 1,
     imageUrl: '',
     description: ''
 };
@@ -19,20 +21,25 @@ const formInitialState = {
 export default function CreatePicture() {
     const navigate = useNavigate();
     const [formValues, setFormValues] = useState(formInitialState);
+    const [formErrors, setFormErrors] = useState({});
+
+    const [isSubmitrd, setIsSubmited] = useState(false);
+    //const [isError, setIsError] = useState(false);
+
+    // useEffect(() => {
+    //     if(Object.keys(error).length !== 0){
+    //         setIsError(true);
+    //     } else {
+    //         setIsError(false);
+    //     }
+    // }, [error]);
+
 
     const changeHandler = (e) => {
-        let isEmpty = false;
-        if(e.target.value !== ""){
             setFormValues(state => ({
                 ...state,
                 [e.target.name]: e.target.value
-            }
-            ));
-        } else {
-            isEmpty = true;
-            return;
-        }
-        
+            }))
     };
 
     const resetFormHandler = () => {
@@ -40,15 +47,35 @@ export default function CreatePicture() {
     };
 
     const submitHandler = async () => {
+        console.log(formValues)
+        // if ( formValues.title === '' ||
+        //      formValues.category === '' ||
+        //      formValues.painter === '' ||
+        //      formValues.imageUrl === '' ||
+        //      formValues.description === '') {
+        //         console.log('inreturn')
+        //          navigate(Path.Gallery);
+        //          return;
+        // }
+
         try {
             const response = await pictureService.create(formValues);
             resetFormHandler();
             navigate(Path.Gallery);
-        } catch (err) {
-            //add error notification
-            console.log(err);
+        } catch (error) {
+            <Modal {...error}/>
+            //setIsError(true);
+            console.log(error);
         }
     };
+
+    const validateInput = (e) =>{
+        e.preventDefault();
+        //console.log(values)
+        setFormErrors(validate(formValues));
+        //setIsSubmited(true);
+         
+    }
 
 {/* -lekciq forms 1:02 controlled forms
     - add onblur for validation for better UI
@@ -56,55 +83,70 @@ export default function CreatePicture() {
 */}
     return (
         <section className={styles["create-page"]}>
-            <form method="post" id="create" >
+            <form method="POST">
                     <h1>Create New Paint</h1>
                     <label htmlFor="title">Picture title:</label>
                     <input  type="text" 
                             id="title" 
                             name="title" 
                             value={formValues.title}
-                            onBlur={() => console.log('on blur function for validation')}
                             onChange={changeHandler}
+                            onBlur={validateInput}
                             placeholder="Enter picture title..." 
                     />
+                    <p className={styles["error-msg"]}>{formErrors.title}</p>
                     <label htmlFor="category">Picture category:</label>
                     <input  type="text"
                             id="category" 
                             name="category" 
                             value={formValues.category}
                             onChange={changeHandler}
+                            onBlur={validateInput}
                             placeholder="Enter picture category..." 
                     />
+                    <p className={styles["error-msg"]}>{formErrors.category}</p>
                     <label htmlFor="painter">Painter name:</label>
                     <input  type="text" 
                             id="painter" 
                             name="painter" 
                             value={formValues.painter}
                             onChange={changeHandler}
+                            onBlur={validateInput}
                             placeholder="Enter painter name..." 
                     />
+                    <p className={styles["error-msg"]}>{formErrors.painter}</p>
                     <label htmlFor="painterAge">Painter age:</label>
                      <input  type="number" 
                             id="painterAge" 
+                            className={styles["age-input"]}
                             name="painterAge" 
-                            value={formValues.painter}
+                            value={formValues.painterAge}
                             onChange={changeHandler}
-                            placeholder="Enter painter age..." 
+                            onBlur={validateInput}
+                            placeholder="1..." 
+                            min={1}
+                            max={75}
                     />
+                    <p className={styles["error-msg"]}>{formErrors.painterAge}</p>
                     <label htmlFor="imageUrl">Picture URL:</label>
                     <input  type="text" 
                             id="imageUrl" 
                             name="imageUrl" 
                             value={formValues.imageUrl}
                             onChange={changeHandler}
+                            onBlur={validateInput}
                             placeholder="Upload a photo..." 
                     />
+                    <p className={styles["error-msg"]}>{formErrors.imageUrl}</p>
                    <label htmlFor="description">Picture description:</label>
                     <textarea   name="description"
                                 value={formValues.description}
-                                onChange={changeHandler} >
-                    </textarea>
-                <button type="button" onClick={submitHandler}>Create picture</button>
+                                onChange={changeHandler}
+                                onBlur={validateInput}
+                                placeholder="Enter picture description here..."
+                    ></textarea>
+                    <p className={styles["error-msg"]}>{formErrors.description}</p>
+                <button type="submit" onSubmit={submitHandler}>Create picture</button>
                 <button type="button" onClick={resetFormHandler}>Reset</button>
             </form>
         </section>
